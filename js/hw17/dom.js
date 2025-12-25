a521162ea73ea0f4bda810e2287aed967d1b0b42
Mykola Sadlii <mykolac08@gmail.com>
2025-12-25 14:54:33 +0000

    hw17

--- js/hw17/dom.js
+++ js/hw17/dom.js
@@ -1,6 +1,28 @@
+// 1
 const buttonRef = document.querySelector(".button");
 console.log(buttonRef);
 
-const inputRef = document.querySelector("[type='text']")
+const inputRef = document.querySelector("[type='text']");
 console.log(inputRef);
-inputRef.value = "hello"
+inputRef.value = "hello";
+
+//2
+const imageRef = document.querySelector(".image");
+console.log(imageRef);
+imageRef.src =
+  "https://th.bing.com/th/id/R.58df31b932d49da39b2e4fd55efccb3c?rik=afiRmvS4J4rWQA&riu=http%3a%2f%2fwonderfulengineering.com%2fwp-content%2fuploads%2f2014%2f10%2fimage-wallpaper.jpg&ehk=%2fYo3AtlqfEtE198rGCOCBHKR2RLpW5m1G2i8wrLibZY%3d&risl=&pid=ImgRaw&r=0";
+
+// 3
+const linkRef = document.querySelector("a");
+linkRef.href = "https://en-gb.facebook.com/";
+console.log(linkRef);
+
+
+const imageEl = document.querySelector(".image2");
+imageEl.alt = "giraffe"
+console.log(imageEl);
+
+// 4 
+
+const listRef = document.querySelector(".list")
+listRef.firstElementChild.textContent = "hello world";
